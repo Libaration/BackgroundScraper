0a42889dbbef06b3716632ddac9afe5d64f6ef9a
Cristian Deleon <libaration@gmail.com>
2023-05-24 16:42:15 -0400

    ✨ feat: Implement driver locking and logging
    This commit implements a driver locking mechanism using Python's `threading.Lock()` to ensure that only one thread accesses the drivers list at a time. It also adds logging functionality via the `log()` function, which logs messages with a timestamp. The `get_driver()` and `release_driver()` functions have been updated to use the lock and log messages when acquiring/releasing drivers. Additionally, print statements have been added to show the results of scrape attempts and the status of connected clients.

--- backend/lib/main.py
+++ backend/lib/main.py
@@ -3,6 +3,9 @@ import socketio
 import baltimore_county as scraper
 import multiprocessing
 import asyncio
+import threading
+import datetime
+
 
 # Create the Socket.IO server
 io = socketio.AsyncServer(cors_allowed_origins="*")
@@ -13,24 +16,39 @@ io.attach(app)
 num_workers = multiprocessing.cpu_count()  # 8
 
 # Initialize driver-related variables
-drivers = []
 available_drivers = []
 connected_clients = set()
+driver_lock = threading.Lock()
+
+
+def log(*args):
+    """Log a message with a timestamp."""
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    msg = " ".join(str(arg) for arg in args)
+    print(f"[{timestamp}] {msg}")
 
 
 def get_driver():
+    log("Available drivers:", len(available_drivers))
+    log(available_drivers)
+    print("-" * 50)
     """Get an available driver or create a new one if none are available."""
-    if available_drivers:
-        print("Reusing driver")
-        return available_drivers.pop()
-    else:
-        print("Creating new driver")
-        return scraper.start_driver()
+    with driver_lock:
+        if available_drivers:
+            log("Reusing driver")
+            return available_drivers.pop()
+        else:
+            log("Creating new driver")
+            driver = scraper.start_driver()
+            log(f"New driver created: {driver}")
+            return driver
 
 
 def release_driver(driver):
     """Release the driver and make it available for reuse."""
-    available_drivers.append(driver)
+    with driver_lock:
+        available_drivers.append(driver)
+        log(f"Driver released: {driver}")
 
 
 @io.event
@@ -45,7 +63,6 @@ def disconnect(sid):
     """Handle the disconnection of a client."""
     print("disconnect ", sid)
     connected_clients.remove(sid)
-    drivers.clear()
     available_drivers.clear()
 
 
@@ -75,19 +92,27 @@ async def scrape_baltimore_county(sid, data):
             result = await asyncio.to_thread(
                 scraper.scrape_baltimore_county, address, driver
             )
+            print(f"Result for {address}: {result}")
+            print("sid ", sid)
+            print("connected_clients ", connected_clients)
             if sid in connected_clients:
                 await io.emit("baltimore_county_scrape_result", result, room=sid)
+                print(f"Sent result for {address}")
         except Exception as e:
             print(f"Error scraping {address}: {e}")
         finally:
             print(f"Releasing driver for {address}")
-            release_driver(driver)
 
     tasks = []
     for address in addresses:
         tasks.append(scrape_single_address(address))
 
     await asyncio.gather(*tasks)
+
+    # Release the drivers after all tasks are completed
+    for address in addresses:
+        driver = get_driver()
+        release_driver(driver)
 
 
 if __name__ == "__main__":
